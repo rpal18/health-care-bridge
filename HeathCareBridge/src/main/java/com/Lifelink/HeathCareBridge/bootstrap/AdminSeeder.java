179ab1d397deb47545f6ae8ecc39578381c2e4e4
Rohit Pal <palrohitpr03@gmail.com>
2026-06-11 13:45:13 +0530

    fix: use external env file for consistent environment variables

--- HeathCareBridge/src/main/java/com/Lifelink/HeathCareBridge/bootstrap/AdminSeeder.java
+++ HeathCareBridge/src/main/java/com/Lifelink/HeathCareBridge/bootstrap/AdminSeeder.java
@@ -6,6 +6,7 @@ import com.Lifelink.HeathCareBridge.model.Role;
 import com.Lifelink.HeathCareBridge.model.User;
 import com.Lifelink.HeathCareBridge.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -19,12 +20,17 @@ public class AdminSeeder implements CommandLineRunner {
     private PasswordEncoder passwordEncoder;
     @Autowired
     private UserRepository userRepository;
+    @Value("${adminEmail}")
+    private String adminEmail;
+    @Value("${adminPassword}")
+    private String adminPassword;
+    @Value("${adminUsername}")
+    private String adminUserName;
+    @Value("${phoneNumber}")
+    private String phoneNumber;
+
     @Override
     public void run(String... args) throws Exception {
-        String adminUserName = System.getenv("adminUsername");
-        String adminEmail = System.getenv("adminEmail");
-        String adminPassword = System.getenv("adminPassword");
-        String phoneNumber = System.getenv("phoneNumber");
         if(adminEmail == null  || adminPassword == null || adminUserName == null){
             throw new DetailsNotFound("Admin email or password  or user name not found in environment variables");
         }
@@ -36,7 +42,7 @@ public class AdminSeeder implements CommandLineRunner {
         superAdmin.setUserName(adminUserName);
         superAdmin.setEmail(adminEmail);
         superAdmin.setPassword(passwordEncoder.encode(adminPassword));
-        superAdmin.setPhoneNumber("+919876543210");
+        superAdmin.setPhoneNumber(phoneNumber);
         superAdmin.setRoles(Set.of(Role.SYSTEM_ADMIN));
         userRepository.save(superAdmin);
 
